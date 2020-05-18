9f1c5d1e2650f373f1e6d890deed176c84588f90
Bparui <bipashaparui3012@gmail.com>
2020-05-18 22:44:04 +0530

    Final code with changes in command line input situations

--- movement.py
+++ movement.py
@@ -1,15 +1,49 @@
 from getpass import getpass
 import random, os, argparse, numpy as np
 
+'''command line input of game size and winning value'''
 parser = argparse.ArgumentParser()
 parser.add_argument('--w', type=int, default=5,
                         help='What is the board size?')
 parser.add_argument('--n', type=int, default=2048, 
                         help='What is the winning value?')
 args=parser.parse_args()
-wid,limit = args.w,args.n
-game=np.zeros([wid,wid], dtype=int)
+width,limit,ck = args.w,args.n,args.n
 
+if width<=0:
+	print("Invalid game size. Game size set to default i.e 5")
+	wid=5
+else:
+	wid=width
+
+'''checks if winning value is correct or not'''
+def limitcheck(n): 
+    if n <= 0: 
+        return "wrong",1
+    elif n==1:
+    	return False,2
+    while n >= 2:
+            if (n % 2 != 0): 
+                return "wrong",2
+            n = n // 2
+    return "ryt",0
+
+k,c=limitcheck(ck)
+if k=="wrong":
+	if c==1:
+		print('Invalid winning value. Value set to default i.e 2048.')
+		limit=2048
+	else:
+		print('Value is not a power of 2. Winning value set to the nearest power of 2 greater than entered value')
+		v=1
+		n=1
+		while n<limit:
+			n=2**v
+			v=v+1
+		limit=n
+
+'''defines gameboard'''
+game=np.zeros([wid,wid], dtype=int)
 
 def format(curr_game):
     for a in curr_game:
@@ -17,7 +51,7 @@ def format(curr_game):
             print("{}".format(elem).rjust(3), end="")
         print(end="\n")
 
-
+'''spawns random two'''
 def random_twos(curr_game):
 	a = random.randint(0, len(game)-1)
 	b = random.randint(0, len(game)-1)
@@ -27,7 +61,7 @@ def random_twos(curr_game):
 	game[a][b] = 2
 	return curr_game
 
-
+'''ccode to define actions of wasd'''
 def merge(curr_game):
 	lnt=len(curr_game)
 	for j in range(lnt):
@@ -68,12 +102,12 @@ def transpose(curr_game):
 				curr_game[i][j]=temp	
 	return curr_game
 
-
+'''checks winning situation'''
 def win(curr_game):
 	for i in range(len(curr_game)):
 		for j in range(len(curr_game)):
 			if curr_game[i][j]==limit:
-				return "You_won"
+				return "YOU WON"
 	for i in range(len(curr_game)-1):
 		for j in range(len(curr_game)-1):
 			if curr_game[i][j]==curr_game[i][j+1] or curr_game[i][j]==curr_game[i+1][j]:
@@ -86,14 +120,13 @@ def win(curr_game):
 		for j in range(len(curr_game)):
 			if curr_game[j][i]==0:
 				return "ntover"
-	return "You_lost"
+	return "YOU LOST"
 
-
-os.system("cls")
+'''playing of game'''
 game=random_twos(game)
 print(format(game))
 r=win(game)
-if r=="You_won":
+if r=="YOU WON":
 	print(r)
 else:
 	while True:
